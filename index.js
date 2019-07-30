2cdec3a052d9b0049efb4e70ebe1fed5bb96deda
AndriantoAGun <andrianto22491@gmail.com>
2019-07-30 08:22:52 +0700

    Tugas 21 JavaScript
    tugas Niomic tentang JavaScript meliputi modifikasi String menggunakan Replace

---
+++ index.js
@@ -0,0 +1,8 @@
+function replaceKata(){
+var str = "Saya belajar masak nasi goreng";
+console.log(str);
+var strReplace = str.replace("masak", "makan");
+console.log(strReplace);
+}
+
+replaceKata();
